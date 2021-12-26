07df0a00f9c806b9937a8182a75311486d5c4325
Paweł <37457030+Luc33k@users.noreply.github.com>
2021-12-26 13:49:41 +0100

    Update raport.php

--- raport.php
+++ raport.php
@@ -1,3 +1,10 @@
-<?php
-    require_once (['./config.php']);
-?>
+<?php require_once ('./raportScript.php')?>
+
+<form action="./raportScript.php" method="post">
+    Value1: <input type="text" name = "field1" /><br/>
+    Value2: <input type="text" name = "field2" /><br/>
+    Value3: <input type="text" name = "field3" /><br/>
+    Value4: <input type="text" name = "field4" /><br/>
+    Value5: <input type="text" name = "field5" /><br/>
+    <input type="submit" />
+</form>
