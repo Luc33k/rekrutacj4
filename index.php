a9847586b61652fb1c5272b608701f19d8a35f59
Paweł <37457030+Luc33k@users.noreply.github.com>
2021-12-26 13:21:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,10 +19,10 @@
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
 
-    
+    <script src="./assets/js/jquery-3.6.0.min.js"></script>
 
     <!-- Bootstrap core CSS -->
-<link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
       .bd-placeholder-img {
@@ -43,6 +43,7 @@
     
     <!-- Custom styles for this template -->
     <link href="./assets/css/index.css" rel="stylesheet">
+    
   </head>
   <body class="d-flex h-100 text-center text-white bg-dark">
     
@@ -51,75 +52,27 @@
         <div>
           <h3 class="float-md-start mb-0">Witaj: <?php echo $_SESSION["username"]?> </h3>
           <nav class="nav nav-masthead justify-content-center float-md-end">
-            <a class="nav-link active" aria-current="page" href="#">Zmiana hasła</a>
-            <a class="nav-link" href="#">Raport</a>
-            <a class="nav-link" href="#">Zestawienie</a>
-            <a class="nav-link" href = "./logout.php">Wyloguj</a>
+            <a class="nav-link active ajaxTrigger" load = "changePass.php" aria-current="page" >Zmiana hasła</a>
+            <a class="nav-link ajaxTrigger" load = "raport.php">Raport</a>
+            <a class="nav-link ajaxTrigger" href="#">Zestawienie</a>
+            <a class="nav-link ajaxTrigger" href = "./logout.php">Wyloguj</a>
           </nav>
         </div>
       </header>
 
-      <main class="form-changepass">
-    
-        <form  method="POST" name= "f2" onsubmit = "return validation()" action="./changePass.php">
-            
-            <h1 class="h3 mb-3 fw-normal">Tutaj możesz zmienić swoje hasło!</h1>
-
-            <div class="form-floating">
-                <input type="password" class="form-control" id="pass_old" name="pass_old" placeholder="Podaj stare hasło">
-                <label for="pass_old">Stare hasło</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="pass_new" name="pass_new" placeholder="Podaj nowe hasło">
-                <label for="pass_new1">Podaj nowe hasło</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="pass_conf" name="pass_conf" placeholder="Powtórz nowe hasło">
-                <label for="pass_new2">Potwierdź nowe hasło</label>
-            </div>
-
-            
-            <button class="w-100 btn btn-lg btn-primary" type="submit" id="button_submit" name="button_submit">Zmień hasło</button>
-            
-            <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-        </form>
-        <script>  
-          function validation(){  
-          var psOld = document.f2.pass_old.value;  
-          var psNew = document.f2.pass_new.value;
-          var psNewConf = document.f2.pass_conf.value;  
-          
-          if(psOld.length=="" && psNew.length=="" && psNewConf.length == "") {  
-            alert("Wypełnij pola");  
-            return false;  
-          }  
-          else{  
-            if(psOld.length=="") {  
-              alert("Pole stare hasło jest puste");  
-              return false;  
-            }   
-            if (psNew.length=="") {  
-              alert("Pole nowe hasło jest puste");  
-              return false;  
-            }  
-            if (psNewConf.length=="") {  
-              alert("Pole potwierdź hasło jest puste");  
-              return false;  
-            }   
-          }                             
-          }  
-        </script>  
-        <!--
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-        <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-        </p>
-        -->
-      </main>
+      <div id="ajaxContent"></div>
 
       <footer class="mt-auto text-white-50">
         <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
       </footer>
     </div>
+    <script>
+      $(document).ready(function(){
+        $('.ajaxTrigger').click(function(){
+          var pageName = $(this).attr('load');
+          $('#ajaxContent').load('./'+pageName);
+        });
+      });
+    </script>
   </body>
 </html>
