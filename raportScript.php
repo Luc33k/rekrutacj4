90519f88e9e49648a2a9b3b9685682b4b8cb1258
Paweł <37457030+Luc33k@users.noreply.github.com>
2021-12-26 13:49:37 +0100

    Update raportScript.php

--- raportScript.php
+++ raportScript.php
@@ -1,3 +1,10 @@
 <?php
-    require_once (['./config.php']);
+    require_once ('./config.php');
+
+    $grupy = $conn->query("SELECT * FROM grupy_produktow");
+    $produkty = $conn->query("SELECT * FROM produkty");
+    $zamowienia = $conn->query("SELECT * FROM zamowienia");
+
+
+
 ?>
