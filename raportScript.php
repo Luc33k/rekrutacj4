4bd9005f8fad888d2259acf3fa59d66a618b0724
Paweł <37457030+Luc33k@users.noreply.github.com>
2021-12-28 19:20:23 +0100

    Update raportScript.php

--- raportScript.php
+++ raportScript.php
@@ -22,7 +22,26 @@
     zamowienia.ilosc AS zamIlos,
     zamowienia.id_produkt AS zamIdProd
     FROM produkty
+    WHERE zamData BETWEEN $begi AND $end
+    WHERE zamData BETWEEN $startDate AND $endDate
+       WHERE zamData = $startDate
+       WHERE zamowienia.data = $testDate
+
+           WHERE zamowienia.data BETWEEN strval($begi) AND strval($end)
     ");*/
+
+
+    $begi = $_GET['raport_start'];
+    $end = $_GET['raport_end'];
+
+    #$startDate = date(date_create_from_format('Y-m-d', $begi));
+    #$endDate = date(date_create_from_format('Y-m-d', $end));
+
+
+    #$startDate = date('Y-m-d', $begi);
+    #$endDate = date('Y-m-d', $end);
+
+    $testDate = strval('2019-01-01');
 
     $dane = mysqli_query($conn, "SELECT 
     produkty.id AS produktID,
@@ -37,8 +56,12 @@
     zamowienia.data AS zamData,
     zamowienia.id_produkt AS zamIdProd
     FROM produkty  
+    
     INNER JOIN grupy_produktow ON produkty.id_grupa = grupy_produktow.id
     INNER JOIN zamowienia ON produkty.id = zamowienia.id_produkt 
+    WHERE zamowienia.data BETWEEN '$begi' AND '$end' 
+    GROUP BY zamData, grupNazwa
+    ORDER BY zamData, grupNazwa DESC
     ");
 
     if($dane->num_rows > 0){
@@ -65,18 +88,26 @@
             if( mysqli_num_rows( $dane )==0 ){
               echo '<tr><td colspan="4">No Rows Returned</td></tr>';
             }else{
-
+              #echo gettype($row['zamDate']);
+              /*echo $endDate;*/
               while( $row = mysqli_fetch_assoc( $dane )){
-              $brutto = $row['netto'] + ($row['netto'] * ($row['vat']/100));
+                #echo gettype($endDate);
+                #echo gettype($testDate);
+                #echo 'zam data ' . $row['zamData'] . gettype($row['zamData']) . '</br>';
+                #echo gettype($begi);
+                #echo 'begi ' . $begi . gettype($begi) . '</br>';
+                #echo 'testDate ' . $testDate . gettype($testDate) . '</br>';
+                #echo 'test 2019-01-01'  . gettype('2019-01-01') . '</br>';
+                $brutto = $row['netto'] + ($row['netto'] * ($row['vat']/100));
                 echo "<tr><td>{$row['grupNazwa']}</td><td>{$row['zamData']}</td><td>{$row['netto']}</td><td>{$brutto}</td></tr>\n";
                 //echo ($row['vat'] * $row['netto']);
               }
-
+              
           ?>
         </tbody>
       </table>
       <?php
+      }
     }
-}
 
 ?>
