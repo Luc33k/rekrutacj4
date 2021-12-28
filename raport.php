4325026d70a36f5bb77ad13bea14e1de5e7db492
Paweł <37457030+Luc33k@users.noreply.github.com>
2021-12-28 21:52:41 +0100

    Update raport.php

--- raport.php
+++ raport.php
@@ -1,3 +1,6 @@
+<head>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> 
+</head>
 <main class = "raport">
     <form id="formRaport" >
         <label for="start">Od:</label>
@@ -16,8 +19,53 @@
 
     </div>
 
+    <div id='chart_div'>
+
+    </div>
+
 </main>
 <script type="text/javascript">
+
+
+
+    function wykres(){
+          // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    }
+      
+
+    
+   
     //alert('test');
 
 
@@ -67,6 +115,7 @@
                 data: serializedData,
                 success: function(data){
                     $("#ajaxContent").html(data);
+                    wykres();
                 }
             });
 
