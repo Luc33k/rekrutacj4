555c127b439d95e9e53e1cb98c7bf06c844be5be
Paweł <37457030+Luc33k@users.noreply.github.com>
2021-12-28 19:20:27 +0100

    Update raport.php

--- raport.php
+++ raport.php
@@ -1,16 +1,18 @@
-<!--<main class = "form-raport">
+<main class = "form-raport">
     <form action="./raportScript.php" method="GET">
         <label for="start">Od:</label>
-        <input type="date" id="start" name="raport-start"
-        value="2018-07-22"
-        min="2018-01-01" max="2018-12-31">
+        <input type="date" id="start" name="raport_start"
+            value="2019-07-22"
+            min="2019-01-01" max="2021-11-24">
         <label for="start">Do:</label>
-        <input type="date" id="end" name="raport-end"
-        value="2018-07-22"
-        min="2018-01-01" max="2018-12-31">
+
+        <input type="date" id="end" name="raport_end"
+            value="2019-07-22"
+            min="2019-01-01" max="2021-11-24">
+        <button class="w-50 btn btn-md btn-primary" type="submit" id="button_submit" name="button_submit">Zaloguj</button>
     </form>
 </main>
--->
 
-<?php require_once ('./raportScript.php')?>
+
+<?# require_once ('./raportScript.php')?>
 
